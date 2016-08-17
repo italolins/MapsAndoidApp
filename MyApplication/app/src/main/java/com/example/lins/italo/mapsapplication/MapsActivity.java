479580e75465ee2527b0226478f5b6174396cdb5
italolins <italo.lins@ccc.ufcg.edu.br>
2016-08-17 16:00:46 -0300

    o marcador foi movido para Campina Grande e a aplicação da um zoom na localização logo no inicio

--- MyApplication/app/src/main/java/com/example/lins/italo/mapsapplication/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/lins/italo/mapsapplication/MapsActivity.java
@@ -2,15 +2,20 @@ package com.example.lins.italo.mapsapplication;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
 
@@ -30,8 +35,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // campina grande (-7.227384, -35.884497)
+
+        LatLng position = new LatLng(-7.227384, -35.884497);
+
+        final CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(position)   // onde a camera ta apontando
+                .bearing(130)       // direção em graus da camera
+                .tilt(0)            // angulo em graus
+                .zoom(17)           // zoom
+                .build();
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+        mMap.animateCamera(cameraUpdate);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(position).title("Marker in Campina Grande");
+        mMap.addMarker(markerOptions);
+
+        //mMap.addMarker(new MarkerOptions().position(position).title("Marker in Campina Grande"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+    }
+
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        TextView tDebug = (TextView) findViewById(R.id.tdebug);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(latLng);
+        tDebug.setText("Posição: " + latLng);
+//		Toast.makeText(getBaseContext(), "Click: " + latLng, Toast.LENGTH_SHORT).show();
+//		map.animateCamera(update);
     }
 }
